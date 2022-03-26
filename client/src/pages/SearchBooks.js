02b8fca3b146ea1d098395e1ab658ab67ddd2c1a
GrumpyMonk <anhoward1988@gmail.com>
2022-03-26 14:40:51 -0800

    page count and published date renders to search page

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -58,8 +58,9 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
         //TODO add pageCount property
+        pageCount: book.volumeInfo.pageCount,
         //TODO add publishedDate property
-
+        publishedDate: book.volumeInfo.publishedDate
       }));
 
       setSearchedBooks(bookData);
@@ -139,6 +140,8 @@ const SearchBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
                   { /* TODO: show the book's pageCount and published Date */}
+                  <p>Page Count: {book.pageCount}</p>
+                  <p>Published Date: {book.publishedDate}</p>
 
                   <Card.Text>{book.description}</Card.Text>
                   {Auth.loggedIn() && (
